8f777989561c69681f2d165f2cbec8b3788c8a45
Louis Blankemeir <lblankem@torino.stanford.edu>
2022-11-07 20:07:22 -0800

    2d segmentation after spine segmentation

---
+++ abctseg/inference_2d.py
@@ -0,0 +1,64 @@
+from abctseg.models import Models
+
+
+def inference_2d(args, dataset, batch_size, use_pp, num_workers, files, num_gpus):
+    for m_name in args.models:
+        logger.info("Computing masks with model {}".format(m_name))
+
+        model_type: Models = None
+        for m_type in Models:
+            if m_type.model_name == m_name:
+                model_type = m_type
+                break
+        assert model_type is not None
+
+        dataset = Dataset(files, windows=model_type.windows)
+        categories = model_type.categories
+        model = model_type.load_model()
+        if num_gpus > 1:
+            model = dl_utils.ModelMGPU(model, gpus=num_gpus)
+
+        logger.info("<MODEL>: {}".format(m_name))
+        logger.info("Computing segmentation masks using {}...".format(m_name))
+        start_time = perf_counter()
+        _, preds, params_dicts = predict(
+            model,
+            dataset,
+            num_workers=num_workers,
+            use_multiprocessing=num_workers > 1,
+            batch_size=batch_size,
+            use_postprocessing=use_pp,
+            postprocessing_params={"categories": categories},
+        )
+        K.clear_session()
+        logger.info(
+            "<TIME>: Segmentation - count: {} - {:.4f} seconds".format(
+                len(files), perf_counter() - start_time
+            )
+        )
+
+        logger.info("Computing metrics...")
+        start_time = perf_counter()
+        masks = [model_type.preds_to_mask(p) for p in preds]
+        assert len(masks) == len(params_dicts)
+        assert len(masks) == len(files)
+        m_save_name = "/{}".format(m_name)
+        if use_pp:
+            m_save_name += "+pp"
+        for f, pred, mask, params in tqdm(  # noqa: B007
+            zip(files, preds, masks, params_dicts), total=len(files)
+        ):
+            x = params["image"]
+            results = compute_results(x, mask, categories, params)
+            output_file = format_output_path(
+                f, base_dirs=dirs if args.batch else None
+            )
+            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+            sio.dicttoh5(
+                results, output_file, m_save_name, mode="a", overwrite_data=True
+            )
+        logger.info(
+            "<TIME>: Metrics - count: {} - {:.4f} seconds".format(
+                len(files), perf_counter() - start_time
+            )
+        )
