b1aa9ae7dc68d1f4b6df454611a1304d4877fe5b
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-03-09 11:47:16 -0800

    organization

--- comp2comp/io/io.py
+++ comp2comp/io/io.py
@@ -9,7 +9,7 @@ from comp2comp.inference_class_base import InferenceClass
 class DicomLoader(InferenceClass):
     """Load a single dicom series.
     """
-    def __init__(self, input_path: Union[str, Path]) -> Dict[str, dm.MedicalVolume]:
+    def __init__(self, input_path: Union[str, Path]):
         super().__init__()
         self.dicom_dir = Path(input_path)
         self.dr = dm.DicomReader()
@@ -30,3 +30,24 @@ class NiftiSaver(InferenceClass):
         nifti_file = inference_pipeline.output_dir
         self.nw.write(medical_volume, nifti_file)
         return {"nifti_file": nifti_file}
+
+class DicomFinder(InferenceClass):
+    """Find dicom files in a directory.
+    """
+    def __init__(self, input_path: Union[str, Path]) -> Dict[str, Path]:
+        super().__init__()
+        self.input_path = Path(input_path)
+
+    def __call__(self, inference_pipeline) -> Dict[str, Path]:
+        """Find dicom files in a directory.
+
+        Args:
+            inference_pipeline (InferencePipeline): Inference pipeline.
+
+        Returns:
+            Dict[str, Path]: Dictionary containing dicom files.
+        """
+        dicom_files = []
+        for file in self.input_path.glob("**/*.dcm"):
+            dicom_files.append(file)
+        return {"dicom_file_paths": dicom_files}
