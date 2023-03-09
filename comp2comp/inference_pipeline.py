0958121f77feb4c206ffade59d4e7085bd69471e
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-03-08 22:41:26 -0800

    now each subclass can modify and access InferencePipelines class

--- comp2comp/inference_pipeline.py
+++ comp2comp/inference_pipeline.py
@@ -17,7 +17,7 @@ class InferencePipeline:
         if self.config is not None:
             for key, value in self.config.items():
                 setattr(self, key, value)
-                
+
         self.inference_classes = inference_classes
 
     def __call__(self, **kwargs):
@@ -29,12 +29,18 @@ class InferencePipeline:
 
         output = kwargs
         for inference_class in self.inference_classes:
-            assert set(inspect.signature(inference_class).parameters.keys()) == set(output.keys()), \
+
+            function_keys = set(inspect.signature(inference_class).parameters.keys())
+            function_keys.remove("inference_pipeline")
+
+            assert function_keys == set(output.keys()), \
                 "Input to inference class, {}, does not have the correct parameters".format(inference_class.__repr__())
 
             print("Running {} with input keys {}".format(inference_class.__repr__(),
                 inspect.signature(inference_class).parameters.keys()))
+
             output = inference_class(self, **output)
+
             print("Finished {} with output keys {}\n".format(inference_class.__repr__(), 
                 output.keys()))
 
@@ -48,19 +54,20 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dicom_dir", type=str, required=True)
-
-    default_output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-    if not os.path.exists(os.path.join(default_output_dir, "outputs")):
-        os.mkdir(os.path.join(default_output_dir, "outputs"))
-    default_output_dir = os.path.join(default_output_dir, "outputs/test.nii.gz")
-
-    parser.add_argument("--output_dir", type=str, default=default_output_dir)
     args = parser.parse_args()
 
+    output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../outputs")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    output_file_path = os.path.join(output_dir, "test.nii.gz")
+
     pipeline = InferencePipeline([
-                                DicomLoader(args.dicom_dir),
-                                NiftiSaver(args.output_dir)
-                                ])
+        DicomLoader(args.dicom_dir),
+        NiftiSaver()
+    ],
+    config={
+        "output_dir": output_file_path
+    })
     pipeline()
 
     print("Done.")
--- comp2comp/io.py
+++ comp2comp/io.py
@@ -4,7 +4,7 @@ from typing import Dict, Union
 from typing import Type
 
 from comp2comp.inference_class_base import InferenceClass
-from comp2comp.inference_pipeline import InferencePipeline
+#from comp2comp.inference_pipeline import InferencePipeline
 
 class DicomLoader(InferenceClass):
     """Load a single dicom series.
@@ -14,19 +14,19 @@ class DicomLoader(InferenceClass):
         self.dicom_dir = Path(input_path)
         self.dr = dm.DicomReader()
 
-    def __call__(self, inference_pipeline: Type[InferencePipeline]) -> Dict:
+    def __call__(self, inference_pipeline) -> Dict:
         medical_volume = self.dr.load(self.dicom_dir, group_by=None, sort_by="InstanceNumber")[0]
         return {"medical_volume": medical_volume}
 
 class NiftiSaver(InferenceClass):
     """Save dosma medical volume object to NIfTI file.
     """
-    def __init__(self, output_path: Union[str, Path]):
+    def __init__(self):
         super().__init__()
-        self.output_dir = Path(output_path)
+        #self.output_dir = Path(output_path)
         self.nw = dm.NiftiWriter()
 
-    def __call__(self, inference_pipeline: Type[InferencePipeline], medical_volume: dm.MedicalVolume) -> Dict[str, Path]:
-        nifti_file = self.output_dir / ("test" + ".nii.gz")
+    def __call__(self, inference_pipeline, medical_volume: dm.MedicalVolume) -> Dict[str, Path]:
+        nifti_file = inference_pipeline.output_dir
         self.nw.write(medical_volume, nifti_file)
         return {"nifti_file": nifti_file}
