dbbcda691f7463d2f2bd880562d899f0fa31dbe2
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-04-02 14:19:48 -0700

    process change

--- comp2comp/utils/process.py
+++ comp2comp/utils/process.py
@@ -1,10 +1,10 @@
 import os
+import shutil
 import sys
 import traceback
 from datetime import datetime
 from pathlib import Path
 from time import time
-import shutil
 
 from comp2comp.io.io_utils import get_dicom_paths_and_num
 
