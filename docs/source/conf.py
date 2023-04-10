6a2211663b048189b00c59a4bfd309dd672a11df
Louis Blankemeier <louis.blankemeier@stanford.edu>
2023-04-09 22:04:04 -0700

    Docs (#67)
    * docs
    
    * image sizing
    
    * docs
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme
    
    * docs
    
    * readme doc
    
    * conf edit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,6 @@ author = 'StanfordMIMI'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
-
 templates_path = ['_templates']
 exclude_patterns = []
 
